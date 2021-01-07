5c9400ddc7019f1f668ec9062a63e007e1bf6378
Josh <josh.lerner95@gmail.com>
2021-01-07 17:43:40 +0000

    Feature: admin Categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,35 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :categories], notice: 'category deleted!'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :category_id,
+    )
+  end
+end 
