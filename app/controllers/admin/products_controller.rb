725396e99b779cb578cee9f62e0f3d65eeda304e
Josh <josh.lerner95@gmail.com>
2021-01-11 19:10:06 +0000

    user authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  before_filter :authorize
+
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD'], except: :index
   
   def index
