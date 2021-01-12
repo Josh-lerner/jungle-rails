6e74d9b6143eaf9f9b37653531d2d43344a2f386
Josh <josh.lerner95@gmail.com>
2021-01-12 02:04:09 +0000

    Model spec: user. Rspec testing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,58 +4,58 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
 
     it 'should save a valid product' do
-      @category = Category.create(name:"Food")
+      @category = Category.create(name:"food")
       product = Product.new(
-        name: "Tofu",
+        name: "Burgers",
         price: 5.00,
         quantity: 12,
         category_id: @category.id
-        )
-      expect(product).to (be_valid)
+      )
+      expect(product).to be_valid
     end
 
     it 'should not save without valid name' do
-      @category = Category.create(name: "Food")
-      name = Product.new(
+      @category = Category.create(name: "food")
+      product = Product.new(
         name: nil,
         price: 5.00,
         quantity: 12,
         category_id: @category.id
-        )
-      expect(name).to_not (be_valid)
+      )
+      expect(product).to_not be_valid
     end
 
     it 'should not save without valid price' do
-      @category = Category.create(name: "Food")
-      price = Product.new(
-        name: "Tofu",
+      @category = Category.create(name: "food")
+      product = Product.new(
+        name: "Burgers",
         price_cents: nil,
         quantity: 12,
         category_id: @category.id
-        )
-      expect(price).to_not (be_valid)
+      )
+      expect(product).to_not be_valid
     end
 
     it 'should not save without valid quantity' do
-      @category = Category.create(name: "Food")
-      quantity = Product.new(
-        name: "Tofu",
+      @category = Category.create(name: "food")
+      product = Product.new(
+        name: "Burgers",
         price: 5.00,
         quantity: nil,
         category_id: @category.id
-        )
-      expect(quantity).to_not (be_valid)
+      )
+      expect(product).to_not be_valid
     end
 
     it 'should not save without valid category id' do
-      @category = Category.create(name: "Food")
-      category_id = Product.new(
-        name: "Tofu",
+      @category = Category.create(name: "food")
+      product = Product.new(
+        name: "Burgers",
         price: 5.00,
         quantity: 12,
         category_id: nil
-        )
-      expect(category_id).to_not (be_valid)
+      )
+      expect(product).to_not be_valid
     end
   end
-end 
+end
